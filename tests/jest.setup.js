be50dc71559bfe18f8b4519f9e9ff4b97372e11f
Yxtan0204 <24006454@myrp.edu.sg>
2026-02-02 01:45:42 +0800

    ....fix

--- tests/jest.setup.js
+++ tests/jest.setup.js
@@ -1,5 +1,11 @@
-require('dotenv').config({ path: '.env.test' });
 //This pool is shared by all test files during the test run
+require('dotenv').config({ path: '.env.test' });
+
+//Load .env.test only for local testing and skip it in CI to avoid overriding GitHub secrets
+if (!process.env.CI) {
+  require('dotenv').config({ path: '.env.test' });
+}
+
 const { pool } = require("../app");
 
 //This runs once after all test files have finished and closes all db connections opened by this test run
